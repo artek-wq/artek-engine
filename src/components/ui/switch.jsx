e02220c1e389d0e40dc214afb89e950691ebcf27
Oso H <administracion@arteklogistic.com>
2026-03-10 07:17:12 -0600

    fix missing switch component

---
+++ src/components/ui/switch.jsx
@@ -0,0 +1,16 @@
+import * as React from "react";
+
+export function Switch({ checked, onCheckedChange }) {
+    return (
+        <button
+            onClick={() => onCheckedChange(!checked)}
+            className={`w-10 h-6 flex items-center rounded-full p-1 transition ${checked ? "bg-blue-600" : "bg-gray-300"
+                }`}
+        >
+            <div
+                className={`bg-white w-4 h-4 rounded-full shadow transform transition ${checked ? "translate-x-4" : ""
+                    }`}
+            />
+        </button>
+    );
+}
